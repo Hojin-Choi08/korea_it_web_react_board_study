ada8602329ba1116e969454d8853e177b4d23560
Hojin-Choi08 <merro3043@gmail.com>
2025-09-03 19:44:28 -0700

    www'
    quit
    exit

--- src/components/MyBoard/MyBoard.jsx
+++ src/components/MyBoard/MyBoard.jsx
@@ -1,17 +1,47 @@
 /** @jsxImportSource @emotion/react */
+import { useEffect, useState } from "react";
 import * as s from "./styles";
+import { getBoardList } from "../../apis/board/board.Apis";
+import { useNavigate } from "react-router-dom";
 
-function MyBoard() {
+function MyBoard({ userId }) {
+  const [boardList, setBoardList] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getBoardList().then((response) => {
+      if (response.data.status === "success") {
+        setBoardList(
+          response.data.data.filter((board) => board.userId === userId)
+        );
+      } else if (response.data.status === "failed") {
+        setBoardList([]);
+        setMessage(response.data.message);
+      }
+    });
+  }, [userId]);
+
   return (
     <div css={s.container}>
       <ul>
-        <li>
-          <div>
-            <span>1.</span>
-            <strong>title</strong>
-          </div>
-          <span>date</span>
-        </li>
+        {boardList.map((board, index) => {
+          const date = board.createDt;
+          const formattedDate = date.split("T")[0];
+          return (
+            <li
+              key={board.boardId}
+              onClick={() => navigate(`/board/${board.boardId}`)}
+            >
+              <div>
+                <span>{index + 1}</span>
+                <strong>{board.title}</strong>
+              </div>
+              <span>{formattedDate}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
